d2cbfcca221c86d266e7bc65eaf36c3feedd1196
Anika Boettcher <10945351@my.uvu.edu>
2021-11-12 15:24:26 -0700

    Created Spawn Manager

---
+++ Personal Project/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject wallPrefab;
+    private Vector3 spawnPos = new Vector3(5, 0, 15)
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(wallPrefab, spawnPos, wallPrefab.transform.rotation); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
