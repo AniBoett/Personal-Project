8d6f4f2e25fd07fb96dec079642fbb3d94e23dbe
Anika Boettcher <10945351@my.uvu.edu>
2021-11-12 16:40:55 -0700

    Improved wall mechanics
    Multiple walls spawn, walls are destroyed when they go out of bounds

--- Personal Project/Assets/Scripts/SpawnManager.cs
+++ Personal Project/Assets/Scripts/SpawnManager.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject wallPrefab;
+    public GameObject[] wallPrefabs;
     private Vector3 spawnPos = new Vector3(5, 1, 15);
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(wallPrefab, spawnPos, wallPrefab.transform.rotation);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int wallIndex = Random.Range(0, wallPrefabs.Length);
+            Instantiate(wallPrefabs[wallIndex], spawnPos, wallPrefabs[wallIndex].transform.rotation);
+        }
     }
 }
