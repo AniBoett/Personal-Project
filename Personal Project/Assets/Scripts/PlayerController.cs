dac2bc544ecb9a9b9c96fe9ac36511b9f9f6f1d9
Anika Boettcher <10945351@my.uvu.edu>
2021-11-19 18:54:09 -0700

    Player controller script bug fixed
    I accidentally dragged the file out of my game. oops

---
+++ Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+
+{
+    public float horizontalInput;
+    public float forwardInput;
+    public float speed = 10.0f;
+
+    public float xRange = 25;
+    public float zRange = 10;
+    public bool gameOver = false;
+    public bool hasPowerup;
+    private Rigidbody playerRb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player can move freely
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * forwardInput * Time.deltaTime * speed);
+        
+
+        //player stays inbounds
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if player collides with wall, game over!
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+    }
+}
