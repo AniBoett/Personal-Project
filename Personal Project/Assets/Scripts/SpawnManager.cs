9b1460c88bc18ab21c144716e730e98c06cdddc7
Anika Boettcher <10945351@my.uvu.edu>
2021-12-14 23:14:57 -0700

    Walls stop spawning once player loses

--- Personal Project/Assets/Scripts/SpawnManager.cs
+++ Personal Project/Assets/Scripts/SpawnManager.cs
@@ -10,11 +10,13 @@ public class SpawnManager : MonoBehaviour
     public GameObject powerupPrefab;
     private float startDelay = 2;
     private float spawnInterval = 2.5f;
+    private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnRandomWall", startDelay, spawnInterval);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -26,7 +28,10 @@ public class SpawnManager : MonoBehaviour
     void SpawnRandomWall()
     //randomly spawn walls
     {
-        int wallIndex = Random.Range(0, wallPrefabs.Length);
-        Instantiate(wallPrefabs[wallIndex], spawnPos, wallPrefabs[wallIndex].transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            int wallIndex = Random.Range(0, wallPrefabs.Length);
+            Instantiate(wallPrefabs[wallIndex], spawnPos, wallPrefabs[wallIndex].transform.rotation);
+        }
     }
 }
