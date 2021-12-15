5988665c3d4a3b1e1e34b7de333ad8a9f354f09b
Anika Boettcher <10945351@my.uvu.edu>
2021-12-14 23:09:24 -0700

    Walls now spawn automatically
    Walls spawn automatically once the game starts

--- Personal Project/Assets/Scripts/SpawnManager.cs
+++ Personal Project/Assets/Scripts/SpawnManager.cs
@@ -8,21 +8,25 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] wallPrefabs;
     private Vector3 spawnPos = new Vector3(5, 3, 15);
     public GameObject powerupPrefab;
+    private float startDelay = 2;
+    private float spawnInterval = 2.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        InvokeRepeating("SpawnRandomWall", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //spawn walls when the 'C' key is pressed; placeholder, eventually walls will spawn automatically
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            int wallIndex = Random.Range(0, wallPrefabs.Length);
-            Instantiate(wallPrefabs[wallIndex], spawnPos, wallPrefabs[wallIndex].transform.rotation);
-        }
+       
+    }
+
+    void SpawnRandomWall()
+    //randomly spawn walls
+    {
+        int wallIndex = Random.Range(0, wallPrefabs.Length);
+        Instantiate(wallPrefabs[wallIndex], spawnPos, wallPrefabs[wallIndex].transform.rotation);
     }
 }
